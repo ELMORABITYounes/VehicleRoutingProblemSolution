92196145cb803551a8ea596a8cbf76202d1d216d
ELMORABITYounes <yelmorabit22@gmail.com>
2019-02-15 00:57:56 +0100

    exposing the link header to the client

--- src/main/java/com/elmorabit/ensak/security/JWTAutorizationFilter.java
+++ src/main/java/com/elmorabit/ensak/security/JWTAutorizationFilter.java
@@ -23,7 +23,7 @@ public class JWTAutorizationFilter extends OncePerRequestFilter {
         httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
         httpServletResponse.addHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
         httpServletResponse.addHeader("Access-Control-Allow-Headers","Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-method, Access-Control-Request-Headers, Authorization");
-        httpServletResponse.addHeader("Access-Control-Expose-Headers","Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
+        httpServletResponse.addHeader("Access-Control-Expose-Headers","Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization, Link, X-Total-Count");
 
         if (httpServletRequest.getMethod().equals("OPTIONS")){
             httpServletResponse.setStatus(HttpServletResponse.SC_OK);
